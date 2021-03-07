29b62de2b0435a2a17e60d961d491e880d3e4358
mandoo <nattty@gmail.com>
2021-03-07 20:49:01 +0900

    20210307

--- backend/post/indicators.py
+++ backend/post/indicators.py
@@ -12,8 +12,9 @@ import pymysql
 from django.core.serializers.json import DjangoJSONEncoder
 import requests
 import json
-
+import math
 import time
+from datetime import date, timedelta
 
 
 class usdkrw(APIView):
@@ -21,48 +22,89 @@ class usdkrw(APIView):
         print('시작')
         apikey = "KDqozvh7wMKStFfUb4EGLkI5iXqgLB8i"
 
-        nal = '20210128'
-        mydate = datetime.strptime(nal, '%Y%m%d')
-        dates = []
-        for i in range(1, 10):
-            beforemonth = mydate + relativedelta(days=-i)
-            strdate = beforemonth.strftime("%Y%m%d")
-            dates.append(strdate)
-        print(dates)
-
         api = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={key}&searchdate={date}&data=AP01"
-        conn = pymysql.connect(host='15.165.162.193', user='root', password='admin1234', db='springyun', charset='utf8',
-                               cursorclass=pymysql.cursors.DictCursor)
-        print('연결성공')
+        sql2 = "SELECT dates FROM usdkrw ORDER BY dates desc LIMIT 1"
+        cursor2.execute(sql2)
+        rows = cursor2.fetchall()
+        print("최근날짜",rows[0]['dates'])
 
-        for i in dates:
-            url = api.format(date=i, key=apikey)
-            res = requests.get(url)
-            data = json.loads(res.text)
+        today = date.today();
+        print('today is ', str(today).replace("-", ""))
+        print("날짜의 차 test", str(rows[0]['dates'] - today)[0])
+        if(str(rows[0]['dates'] - today)[0] != '0'):
 
-            if (len(data) != 0):
-                print('  수치 :', data[-1]["deal_bas_r"].replace(",", ""))
-                print('  날짜 :', i)
-                year = i[:4]
-                month = i[4:6]
-                day = i[-2:]
-                alldate = "{}-{}-{}".format(year, month, day)
-                print(alldate)
+            mydate = datetime.strptime(str(today).replace("-", ""), '%Y%m%d')
+            dates = []
+            for i in range(1, int(str(rows[0]['dates'] - today).split('-')[1].split(' ')[0])):
+                beforemonth = mydate + relativedelta(days=-i)
+                strdate = beforemonth.strftime("%Y%m%d")
+                dates.append(strdate)
 
-                cursor = conn.cursor()
-                sql = "insert into usdkrw(rates, dates) values({}, '{}')".format(
-                    data[-1]["deal_bas_r"].replace(",", ""), alldate)
+            print(dates)
 
-                print(sql)
-                cursor.execute(sql)
-                conn.commit()
-                data = json.dumps(cursor.fetchall(), cls=DjangoJSONEncoder)
-                cursor.close()
+            for i in dates:
+                url = api.format(date=i, key=apikey)
+                res = requests.get(url)
+                data = json.loads(res.text)
+                print("데이터 확인", data)
+                if (len(data) != 0):
 
-                time.sleep(1)
+                    year = i[:4]
+                    month = i[4:6]
+                    day = i[-2:]
+                    alldate = "{}-{}-{}".format(year, month, day)
+                    print(alldate)
+
+                    cursor = conn.cursor()
+                    sql = "insert into usdkrw(price, dates) values({}, '{}')".format(
+                        data[-1]["deal_bas_r"].replace(",", ""), alldate)
+
+                    print(sql)
+                    cursor.execute(sql)
+                    conn.commit()
+                    data = json.dumps(cursor.fetchall(), cls=DjangoJSONEncoder)
+
+                    cursor.close()
+
+                    time.sleep(1)
+
+        else:
+            data = []
+
+        cursor3 = conn.cursor()
+
+        # 테이블 변경
+        sql3 = "select * from usdkrw order by dates asc"
+        cursor3.execute(sql3)
+        data2 = cursor3.fetchall()
+        cursor3.close()
+
+        prev = 0
+        result = []
+        for idx, n in enumerate(data2):
+            if (idx == 0):
+                prev = n['price']
+                continue
+            rates = round(((n['price'] - prev) / prev) * 100, 3)
+            prev = n['price']
+            temp = dict(dates=n['dates'], changedate=rates)
+            result.append(temp)
+
+        cursor4 = conn.cursor()
+
+        for n in result:
+            # 테이블 변경
+            sql4 = "update usdkrw set changedate = {} where dates = '{}'".format(n['changedate'], n['dates'])
+
+            cursor4.execute(sql4)
+            print(sql4)
+            conn.commit()
+        cursor4.close()
+
         conn.close()
         question = models.Question(data=data)
         serializer = serializers.QuestionSerializer(question)
         print("test")
+
         return Response(serializer.data);
 
